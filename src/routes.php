da7cf61bd02cc9a67cf6e384f0189a02db11332b
Bruno Bevilaqua <bbbevilaqua@gmail.com>
2018-04-19 13:59:20 -0300

    Update(Routes)-Alter(function[call controller])

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,4 @@
 <?php
-// validation archive
-require_once('validation_functions.php');
 
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -16,60 +14,43 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
 
 $app->get('/api/user', function (Request $request, Response $response, array $args) {
     $mapper = $this->db;
-
-    require_once('Models/Model.php');
-    $mdl = new Model('User', null, $mapper,'userGetAll');
-    $res_query = $mdl->openModel();
-    if ($res_query) {
+    require_once('Controllers/Controller.php');
+    $ctr = new Controller('User', null, $mapper,'getAll');
+    $res_controller = $ctr->openController();
+    if ($res_controller)
+    {
       $this->logger->info("Get-User: All users");
     }
-    $resp = array('response' => $res_query,'description'=>'All users were request !');
+    $resp = array('response' => $res_controller,'description'=>'All users were request !');
       echo (json_encode($resp));
+      // return(json_encode($resp));
 });
 
 $app->get('/api/user/{id}', function (Request $request, Response $response, array $args) {
-    $mapper = $this->db;
-    $data = $args;
-    $this->logger->info("Get-User: id:".$data['id']);
-    require_once('Models/Model.php');
-    $mdl = new Model('User', $data, $mapper,'userGetOne');
-    $res_query = $mdl->openModel();
-    $resp = array('response' => $res_query,'description'=>'User request !');
+  $mapper = $this->db;
+  $data = $args;
+  require_once('Controllers/Controller.php');
+  $ctr = new Controller('User', $data, $mapper,'getOne');
+  $res_controller = $ctr->openController();
+  if ($res_controller)
+  {
+    $this->logger->info("Get-User: id: ".$data['id']);
+  }
+  $resp = array('response' => $res_controller,'description'=>'A user were request !');
       echo (json_encode($resp));
 });
 
 $app->put('/api/user', function (Request $req,  Response $res, $args = []) {
   $mapper = $this->db;
   $data = $req->getParams();
-  if(!isset($data['name'])){
-    echo json_encode($response = array('description'=>'Name is null !','response' => false));
-    exit;
+  require_once('Controllers/Controller.php');
+  $ctr = new Controller('User', $data, $mapper,'update');
+  $res_controller = $ctr->opencontroller();
+  if ($res_controller)
+   {
+     $this->logger->info("Update-User: id:".$data['id']." Name:".$data['name']." Email:".$data['email']." Admin:".((isset($data['admin']))?$data['admin']:0)."  Active:".((isset($data['admin']))?$data['admin']:0));
   }
-  $data['name'] = ValidationFunctions::remove_special_caracters($data['name']);
-  $validate_email = ValidationFunctions::validate_email($data['email']);
-  if(!$validate_email)
-  {
-    $data['email'] = null;
-  }
-
-  if(!isset($data['admin']))
-  {
-    $data['admin'] = false;
-  }else{
-    $data['admin'] = true;
-  }
-
-  if(!isset($data['active']))
-  {
-    $data['active'] = false;
-  }else{
-    $data['active'] = true;
-  }
-  $this->logger->info("Update-User: id:".$data['id']." Name:".$data['name']." Email:".$data['email']." Admin:".$data['admin']."  Active:".$data['active']);
-  require_once('Models/Model.php');
-  $mdl = new Model('User', $data, $mapper,'userUpdate');
-  $res_query = $mdl->openModel();
-  $resp = array('response' => $res_query,'description'=>'User Update !');
+  $resp = array('response' => $res_controller,'description'=>'User Update !');
     echo (json_encode($resp));
 });
 $app->delete('/api/user/{id}', function (Request $req,  Response $res, $args = []) {
@@ -77,13 +58,16 @@ $app->delete('/api/user/{id}', function (Request $req,  Response $res, $args = [
   $mapper = $this->db;
   $data = $args;
   //logger the post-user
-  $this->logger->info("Delete-User: id:".$data['id']);
-  require_once('Models/Model.php');
-  $mdl = new Model('User', $data, $mapper,'deleteUser');
-  $res_query = $mdl->openModel();
-  $resp = array('response' => $res_query,'description'=>'User delete !');
+  require_once('Controllers/Controller.php');
+  $ctr = new Controller('User', $data, $mapper,'delete');
+  $res_controller = $ctr->openController();
+  if ($res_controller)
+   {
+    $this->logger->info("Delete-User: id:".$data['id']);
+  }
+  $resp = array('response' => $res_controller,'description'=>'User delete !');
     echo (json_encode($resp));
-    //return($response);
+    //return(json_encode($response));
 });
 
 //example post
@@ -92,39 +76,15 @@ $app->post('/api/user', function (Request $req,  Response $res, $args = []) {
   $mapper = $this->db;
   // data
   $data = $req->getParams();
-
-  //start validation
-  if(!isset($data['name'])){
-    echo json_encode($response = array('success' => 'false','description'=>'Name is null !','response' => null));
-    exit;
-  }
-  $data['name'] = ValidationFunctions::remove_special_caracters($data['name']);
-  $validate_email = ValidationFunctions::validate_email($data['email']);
-  if(!$validate_email)
-  {
-    $data['email'] = null;
-  }
-
-  if(!isset($data['admin']))
-  {
-    $data['admin'] = false;
-  }else{
-    $data['admin'] = true;
-  }
-
-  if(!isset($data['active']))
-  {
-    $data['active'] = false;
-  }else{
-    $data['active'] = true;
-  }
-
   //logger the post-user
-  $this->logger->info("Post-User: Name:".$data['name']." Email:".$data['email']." Admin:".$data['admin']."  Active:".$data['active']);
-  require_once('Models/Model.php');
-  $mdl = new Model('User', $data, $mapper,'userRegister');
-  $res_query = $mdl->openModel();
-  $resp = array('response' => $res_query,'description'=>'User register !');
+  require_once('Controllers/Controller.php');
+  $ctr = new Controller('User', $data, $mapper,'create');
+  $res_controller = $ctr->openController();
+  if ($res_controller)
+  {
+     $this->logger->info("Post-User: Name:".$data['name']." Email:".$data['email']." Admin:".((isset($data['admin']))?$data['admin']:0)."  Active:".((isset($data['admin']))?$data['admin']:0));
+  }
+  $resp = array('response' => $res_controller,'description'=>'User register !');
     echo (json_encode($resp));
-    //return($response);
+    //return(json_encode($response));
 });

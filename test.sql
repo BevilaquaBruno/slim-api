a690b7efd13697e19ed0295990cd5422d5fe5981
Bruno Bevilaqua <bbbevilaqua@gmail.com>
2018-04-27 11:16:27 -0300

    Update(Test.sql)-Alter(Add table products)

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 19-Abr-2018 às 21:08
+-- Generation Time: 27-Abr-2018 às 16:13
 -- Versão do servidor: 10.1.30-MariaDB
 -- PHP Version: 7.1.14
 
@@ -21,6 +21,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `test`
 --
+CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `test`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `type_id` int(11) NOT NULL,
+  `entry_date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_type` (`type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `type_id`, `entry_date`) VALUES
+(1, 'product1', 1, '2018-04-27');
 
 -- --------------------------------------------------------
 
@@ -28,25 +52,26 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `type`
 --
 
-CREATE TABLE `type` (
-  `id` int(11) NOT NULL,
-  `description` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `type`
 --
 
 INSERT INTO `type` (`id`, `description`) VALUES
-(1 , 'Type 1'),
-(2 , 'Type 2'),
-(3 , 'Type 3'),
-(4 , 'Type 4'),
-(5 , 'Type 5'),
-(6 , 'Type 6'),
-(7 , 'Type 7'),
-(8 , 'Type 8'),
-(9 , 'Type 9'),
+(1, 'Type 1'),
+(2, 'Type 2'),
+(3, 'Type 3'),
+(4, 'Type 4'),
+(5, 'Type 5'),
+(6, 'Type 6'),
+(7, 'Type 7'),
+(8, 'Type 8'),
+(9, 'Type 9'),
 (10, 'Type 10'),
 (11, 'Type 11'),
 (12, 'Type 12'),
@@ -60,58 +85,43 @@ INSERT INTO `type` (`id`, `description`) VALUES
 -- Estrutura da tabela `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `admin` tinyint(1) NOT NULL DEFAULT '0',
   `active` tinyint(1) NOT NULL DEFAULT '1',
-  `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `password` varchar(100) NOT NULL,
+  `token` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `user`
 --
-INSERT INTO `user` (`id`, `name`, `email`, `admin`, `active`,`password`) VALUES
-(1, 'Clark Kent', 'iam@superman.com', 1, 1, '202cb962ac59075b964b07152d234b70'),
-(2, 'Bruce Wayne', 'iam@batman.com', 0, 1, '202cb962ac59075b964b07152d234b70'),
-(3, 'Diana Prince', 'iam@wonderwoman.com', 1, 1, '202cb962ac59075b964b07152d234b70'),
-(4, 'Barry Allen', 'iam@thefastestmanalive.com', 0, 1, '202cb962ac59075b964b07152d234b70'),
-(5, 'Jay Garrick', 'iam@therealflash.com', 1, 1, '202cb962ac59075b964b07152d234b70'),
-(6, 'Wally West', 'iam@thefakeflash.com', 0, 1, '202cb962ac59075b964b07152d234b70'),
-(7, 'Bart Allen', 'iam@thesimpsonsflash', 1, 1, '202cb962ac59075b964b07152d234b70'),
-(8, 'Tony Stark', 'iam@ironman.com', 1, 1, '202cb962ac59075b964b07152d234b70');
+
+INSERT INTO `user` (`id`, `name`, `email`, `admin`, `active`, `password`, `token`) VALUES
+(1, 'Clark Kent', 'iam@superman.com', 1, 1, '202cb962ac59075b964b07152d234b70', NULL),
+(2, 'Bruce Wayne', 'iam@batman.com', 0, 1, '202cb962ac59075b964b07152d234b70', NULL),
+(3, 'Diana Prince', 'iam@wonderwoman.com', 1, 1, '202cb962ac59075b964b07152d234b70', NULL),
+(4, 'Barry Allen', 'iam@thefastestmanalive.com', 0, 1, '202cb962ac59075b964b07152d234b70', NULL),
+(5, 'Jay Garrick', 'iam@therealflash.com', 1, 1, '202cb962ac59075b964b07152d234b70', NULL),
+(6, 'Wally West', 'iam@thefakeflash.com', 0, 1, '202cb962ac59075b964b07152d234b70', NULL),
+(7, 'Bart Allen', 'iam@thesimpsonsflash', 1, 1, '202cb962ac59075b964b07152d234b70', NULL),
+(8, 'Tony Stark', 'iam@ironman.com', 1, 1, '202cb962ac59075b964b07152d234b70', NULL),
+(9, 'UserNames', 'user@email.com', 1, 1, '202cb962ac59075b964b07152d234b70', NULL),
+(10, 'UserNames', 'user@email.com', 1, 1, '202cb962ac59075b964b07152d234b70', NULL),
+(11, 'UserNames', 'user@email.com', 1, 1, '202cb962ac59075b964b07152d234b70', NULL);
+
 --
--- Indexes for dumped tables
+-- Constraints for dumped tables
 --
 
 --
--- Indexes for table `type`
+-- Limitadores para a tabela `products`
 --
-ALTER TABLE `type`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `type`
---
-ALTER TABLE `type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `products`
+  ADD CONSTRAINT `id_type` FOREIGN KEY (`type_id`) REFERENCES `type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
